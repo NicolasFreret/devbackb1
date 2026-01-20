108e0b8b9b774286cf503d13cd08df5ecd036063
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2026-01-20 17:08:08 +0100

    updates

--- Controllers/404.php
+++ Controllers/404.php
@@ -1,4 +1,13 @@
 <?php
+
+ $users = read('villes_france_free',
+        [
+            'fields'=>'ville_nom, ville_departement',
+            'conditions' => 'INNER JOIN departement ON ville_departement = departement_code'
+        ]
+ );
+
+ print_r($users);
     
 return [
     'title' => "404"
--- Models/crud.php
+++ Models/crud.php
@@ -1,5 +1,25 @@
 <?php
 
+     /**
+      * @link https://phpdelusions.net/pdo_examples/
+      */
+
+     function bddConnect(){
+          $servername = getConfig('bdd')['host'];
+          $username = getConfig('bdd')['user'];
+          $password = getConfig('bdd')['password'];
+          $bddName = getConfig('bdd')['bddName'];
+
+
+          try {
+               return new PDO("mysql:host=$servername;dbname=".$bddName, $username, $password);
+          } catch(PDOException $e) {
+               echo "Connection failed: " . $e->getMessage();
+          }
+     }
+
+
+
 
      function getUsers(){
           return json_decode( 
@@ -12,7 +32,14 @@
 
      }
 
-     function read(){
+     function read(string $table, array $_options=[]):array{
+       $options = [
+          "fields"=>'*',
+          "conditions" => "",
+          ...$_options
+       ];
+        $bdd = bddConnect();
+        return $bdd->query("SELECT ".$options['fields']." FROM $table ".$options['conditions'])->fetchAll();
         
      }
 
